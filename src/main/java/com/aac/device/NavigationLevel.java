c5a79d44bc52e32bfd6f1cb8aa2c471395d07142
Cassie Mai <cassiemai@Cassies-MBP-2.hsd1.tx.comcast.net>
2025-02-06 20:20:39 -0600

    Update NavigationLevel

--- src/main/java/com/aac/device/NavigationLevel.java
+++ src/main/java/com/aac/device/NavigationLevel.java
@@ -1,30 +1,7 @@
 package com.aac.device;
 
 public enum NavigationLevel {
-    CATEGORY_GROUP("category_group"),
-    CATEGORY("category"),
-    CARD("card");
-
-
-    private final String value;
-
-    NavigationLevel(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public static NavigationLevel forValue(String value) {
-
-        if (value == null || value.isBlank()) return NavigationLevel.CATEGORY_GROUP;
-
-        for (NavigationLevel type : values()) {
-            if (type.getValue().equalsIgnoreCase(value)) {
-                return type;
-            }
-        }
-        return NavigationLevel.CATEGORY_GROUP;
-    }
+    CATEGORY_GROUP,
+    CATEGORY,
+    CARD;
 }
